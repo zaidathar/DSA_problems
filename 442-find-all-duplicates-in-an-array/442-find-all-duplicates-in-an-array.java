c1f3c4e940141d21a34c6d6874a921776a8da45d
Zaid Athar <107996962+zaidathar@users.noreply.github.com>
2022-06-27 11:18:29 +0530

    Time: 70 ms (5.19%), Space: 70 MB (16.82%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
@@ -1,6 +1,23 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        // first Brute force algorithm 
+        //second using hashmap O(n)
+        List<Integer> numbersAppearTwice = new ArrayList<>();
+        HashMap<Integer , Integer> map = new HashMap<>();
+        
+        for(int i = 0;i< nums.length ;i++){
+            map.put(nums[i],map.get(nums[i])==null ? 1 : map.get(nums[i])+1);
+            
+        }
+        for(Map.Entry mp : map.entrySet()){
+            if(mp.getValue().equals(Integer.valueOf(2))){
+                numbersAppearTwice.add((Integer)mp.getKey());
+            }
+        }
+        
+        return numbersAppearTwice;
+        
+        /*
+         //first Brute force algorithm O(nlogn)
         Arrays.sort(nums);
         List<Integer> numbersAppearTwice = new ArrayList<>();
         for(int i = 1 ;i<nums.length ;i+=1){
@@ -8,5 +25,6 @@ class Solution {
             
         }
         return numbersAppearTwice;
+        */
     }
 }
