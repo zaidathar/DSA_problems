affddcb7324e93a0619de790f29ccb23c96e65ba
Zaid Athar <107996962+zaidathar@users.noreply.github.com>
2022-06-29 22:43:07 +0530

    Time: 1 ms (100.00%), Space: 47.8 MB (46.22%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,5 +1,16 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        //Better approach with space O(1)
+        int i =0 ;
+        
+        for(int num : nums){
+            if(i==0 || num > nums[i-1] ){
+                nums[i++]=num;
+            }
+        }
+        return i;
+        
+        /* Space complexity is O(n)
         int[] res = new int[nums.length];
         res[0]=nums[0];
         int cnt = 1 , j =1;
@@ -13,6 +24,8 @@ class Solution {
         }
         for(int i = 0 ; i<cnt;i++)nums[i]=res[i];
         return cnt;
+        
+        */
         
     }
     
