e133e16d67799b17cfc1a11eea38a05b1a5e330e
Zaid Athar <107996962+zaidathar@users.noreply.github.com>
2022-07-06 22:01:09 +0530

    Time: 1 ms (45.21%), Space: 41.1 MB (28.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,8 +1,7 @@
 class Solution {
+    public ArrayList<Integer> fib = new ArrayList<>(Arrays.asList(0,1));
     public int fib(int n) {
-        ArrayList<Integer> fib = new ArrayList<>(Arrays.asList(0,1));
         if(n<=1)return fib.get(n);
-        
         for(int i = 2 ; i<=n;i++){
             fib.add(fib.get(i-1)+fib.get(i-2));
         }
